057311aa32f5afb4601b6598e7c34934d23a9f21
Bo Yan <boyan@umail.ucsb.edu>
2016-02-02 11:18:33 -0800

    Initial Commit

---
+++ Huffman/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Huffman
+//
+//  Created by Bo Yan on 2/2/16.
+//  Copyright © 2016 Bo_Yiting. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
