e4184ca79c9fe25eb0b2de3a67ba6e8ea188ba35
Yiting Ju <yiting.ju@gmail.com>
2016-02-10 16:30:33 -0800

    Finish decoding

--- Huffman/Preprocess.cpp
+++ Huffman/Preprocess.cpp
@@ -10,28 +10,36 @@
 #include <iostream>
 #include <fstream>
 #include "Preprocess.h"
+using namespace std;
 
 Preprocess::Preprocess() {
+    for(int i=0; i<27; i++)
+    {
+        freqArray[i] = 0;
+    }
     
 }
 
 void Preprocess::readFile() {
-    std::ifstream in("plain.input");
+    std::ifstream in("/home/yiting/Dropbox/Winter2016/CMPSC130A/project2/Huffman-master/Huffman/plain.input");
     std::string contents((std::istreambuf_iterator<char>(in)),
                          std::istreambuf_iterator<char>());
 //    std::cout << contents.c_str() << std::endl;
     inputStr = contents.c_str();
-//    std::cout << inputStr << std::endl;
+    //std::cout << inputStr << std::endl;
 }
 void Preprocess::countFreq() {
-    for (int i = 0; i < inputStr.size(); i++) {
+    for (int i = 0; i < inputStr.size()-1; i++) {   //-1 for not counting the ending symbol
+        cout << inputStr[i] << endl;
         // lower letters
         if((inputStr[i] - 'a') >= 0) {
             freqArray[(inputStr[i] - 'a')]++;
+            cout << "aaa" << endl;
         }
         // space
         else {
             freqArray[26]++;
+            cout << "bbb" << endl;
         }
         
     }
