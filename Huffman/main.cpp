bb848244fe26d7c5150b698901ab29a8e9851048
Yiting Ju <yiting.ju@gmail.com>
2016-02-10 16:32:02 -0800

    Finish decoding

--- Huffman/main.cpp
+++ Huffman/main.cpp
@@ -12,31 +12,51 @@
 
 using namespace std;
 int main(int argc, const char * argv[]) {
-//    Preprocess test;
-//    test.readFile();
+   Preprocess test;
+   test.readFile();
+   test.countFreq();
+    // int fArray[27]= {1,0,0,0,2,0,0,0,2,0,1,2,0,0,0,2,0,0,1,0,0,0,0,0,0,0,2};
+    MinHeap mh = *new MinHeap(test.getFreqArray());
+    mh.buildHeap();
+    mh.printMinHeap();
+    cout << endl;
+    mh.constructTrie();
+    mh.huffmanEncode(mh.getRootNode(), new int[30], 0);
+    cout << endl;
+    cout << endl;
+    mh.printCode();
+
 //    cout << (' '-'a') << endl;
 //    cout << char('a' + 1) << endl;
     
-    int fArray[27]= {2,1,4,22,9,6,11,25};
+
+    /*
+    int fArray[27]= {1,0,0,0,2,0,0,0,2,0,1,2,0,0,0,2,0,0,1,0,0,0,0,0,0,0,2};
+    // exactly as shown in the lab instruction
     MinHeap mh = *new MinHeap(fArray);
     mh.buildHeap();
     mh.printMinHeap();
+    cout << endl;
     mh.deleteMin();
     mh.printMinHeap();
+    cout << endl;
     mh.deleteMin();
     mh.printMinHeap();
+    cout << endl;
     mh.deleteMin();
     mh.printMinHeap();
-    MinHeapNode newNode = {'k', 3};
+    cout << endl;
+    MinHeapNode newNode = {'b', 3, NULL, NULL};
     mh.insert(&newNode);
     mh.printMinHeap();
     
     
     
     cout << endl;
-//    mh.constructTrie();
-//    mh.huffmanEncode(mh.getRootNode(), new int[30], 0);
-//    mh.printCode();
+    mh.constructTrie();
+    mh.huffmanEncode(mh.getRootNode(), new int[30], 0);
+    mh.printCode();
+    */
     
     return 0;
 }
